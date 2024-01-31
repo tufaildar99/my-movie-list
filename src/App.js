b15b60e41cca8351312ea790fa66f7ccabed5025
Tufail Dar <tufaildar2016@gmail.com>
2024-01-31 15:57:13 +0530

    Delete unnecessary files and clean up code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useEffect } from "react";
+import StarRating from "./StarRating";
+import img from "./img.jpg";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <NavBar>
+        <Search />
+        <NumResults />
+      </NavBar>
+      <Main>
+        <Box>
+          <MovieList />
+        </Box>
+        <Box>
+          <MovieDetails />
+        </Box>
+      </Main>
     </div>
   );
 }
 
-export default App;
+function NavBar({ children }) {
+  return (
+    <div className="navbar">
+      <Logo />
+      {children}
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+      <span role="img" aria-label="movie">
+        🎬
+      </span>
+      <h4>My Movie List</h4>
+    </div>
+  );
+}
+
+function Search() {
+  return (
+    <div className="search">
+      <input type="text" placeholder="Search Movies" />
+    </div>
+  );
+}
+
+function NumResults() {
+  return (
+    <div className="numresults">
+      <p>Found 3 results</p>
+    </div>
+  );
+}
+
+function Main({ children }) {
+  return <div className="main">{children}</div>;
+}
+
+function Box({ children }) {
+  return <div className="box">{children}</div>;
+}
+
+function MovieList() {
+  return (
+    <ul className="movielist">
+      <Movie />
+      <Movie />
+      <Movie />
+    </ul>
+  );
+}
+
+function Movie() {
+  return (
+    <li className="movie">
+      <img alt="" />
+      <h3>Inception</h3>
+      <div>
+        <p>
+          <span>🗓</span>
+          <span>2010</span>
+        </p>
+      </div>
+    </li>
+  );
+}
+
+function MovieDetails() {
+  return (
+    <div className="details">
+      <header>
+        <img />
+        <div className="details-overview">
+          <h2>Inception</h2>
+          <p>2010 &bull; 128 min</p>
+          <p>Romance</p>
+          <p>
+            <span>⭐️</span>
+            8.8 IMDb rating
+          </p>
+        </div>
+      </header>
+
+      <section>
+        <div className="rating">
+          <>
+            <StarRating />
+          </>
+
+          <p>
+            You rated movie with 10 <span>⭐️</span>
+          </p>
+        </div>
+        <p>
+          <em>
+            A seventeen-year-old aristocrat falls in love with a kind but poor
+            artist aboard the luxurious, ill-fated R.M.S. Titanic.
+          </em>
+        </p>
+        <p>Starring Bret Hart, Jeff Jarrett, Brian James, David Heath</p>
+        <p>Directed by Shane Van Dyke</p>
+      </section>
+    </div>
+  );
+}
